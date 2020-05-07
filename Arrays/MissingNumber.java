1ee0edc885626a78540ada43acee967a744aa6ac
Nidhi Singh <58265877+nids2904@users.noreply.github.com>
2020-05-07 15:54:25 +0530

    Create MissingNumber.java

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class MissingNumber
+ {
+	public static void main (String[] args)
+	 {
+	    Scanner sc= new Scanner(System.in);
+	    int numOfArray = sc.nextInt();
+	 
+	    int[] solution = new int[numOfArray];
+	    
+	    for(int i=0;i<numOfArray;i++){
+	         int arrayLength = sc.nextInt();
+	         int arraySum=0;
+	         for(int j=0;j<arrayLength-1;j++){
+	            arraySum += sc.nextInt();     
+	         }
+	         int expectedSum= arrayLength*(arrayLength+1)/2;
+	         solution[i]=expectedSum-arraySum;
+	    }
+	    
+	    for(int x: solution){
+	        System.out.println(x);
+	    }
+	 }
+}
